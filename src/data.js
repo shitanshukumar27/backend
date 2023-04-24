71eced28a34e010b2129f5f03eb4ac52dbef8aa2
SHITANSHU KUMAR MITHUN <shitanshukumar27@.com>
2023-04-24 21:58:01 +0530

    first commit

---
+++ src/data.js
@@ -0,0 +1,19 @@
+//For creation of subscriber schema lets write the required keys name and subscribed channel;
+
+const data = [
+    {
+      "name": "Jeread Krus",
+      "subscribedChannel": "CNET"
+    },
+    {
+      "name": "John Doe",
+      "subscribedChannel": "freeCodeCamp.org"
+    },
+    {
+      "name": "Lucifer",
+      "subscribedChannel": "Sentex"
+    }
+  ]
+
+  //Export data
+module.exports = data;
